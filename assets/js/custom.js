c7d92e43e2c27bf76bd34bb1fab7f10852e4dd6a
Stephen Young <stephenjayyoung@gmail.com>
2016-06-19 22:51:22 -0400

    changed header to different rev slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,98 @@ jQuery(document).ready(function($){
 
 
 
+    // setup revolution slider
+    $('.tp-banner').show().revolution({
+        sliderType: "standard",
+        jsFileLocation: "assets/include/rs-plugin/js/",
+        sliderLayout: "fullwidth",
+        dottedOverlay: "none",
+        delay: 6000,
+        startwidth: 2000,
+        startheight: 950,
+        hideThumbs: 200,
+        thumbWidth: 100,
+        thumbHeight: 50,
+        thumbAmount: 5,
+        fullScreenAlignForce: "on",
+        navigation: {
+            keyboardNavigation: "off",
+            keyboard_direction: "horizontal",
+            mouseScrollNavigation: "off",
+            onHoverStop: "off",
+            touch:{
+                touchenabled: "on",
+                swipe_threshold: 75,
+                swipe_min_touches: 1,
+                swipe_direction: "horizontal",
+                drag_block_vertical: false
+            },
+            arrows: {
+                style: "hermes",
+                enable: false,
+                hide_onmobile: true,
+                hide_onleave: true,
+                tmp: '<div class="tp-arr-allwrapper">   <div class="tp-arr-imgholder"></div>    <div class="tp-arr-titleholder">{{title}}</div> </div>',
+                left: {
+                    h_align: "left",
+                    v_align: "center",
+                    h_offset: 10,
+                    v_offset: 0
+                },
+                right: {
+                    h_align: "right",
+                    v_align: "center",
+                    h_offset: 10,
+                    v_offset: 0
+                }
+            }
+        },
+        touchenabled: "on",
+        onHoverStop: "on",
+        swipe_velocity: 0.7,
+        swipe_min_touches: 1,
+        swipe_max_touches: 1,
+        drag_block_vertical: false,
+        parallax: "mouse",
+        parallaxBgFreeze: "on",
+        parallaxLevels: [7,4,3,2,5,4,3,2,1,0],
+        keyboardNavigation: "off",
+        navigationHAlign: "center",
+        navigationVAlign: "bottom",
+        navigationHOffset: 0,
+        navigationVOffset: 20,
+        soloArrowLeftHalign: "left",
+        soloArrowLeftValign: "center",
+        soloArrowLeftHOffset: 20,
+        soloArrowLeftVOffset: 0,
+        soloArrowRightHalign: "right",
+        soloArrowRightValign: "center",
+        soloArrowRightHOffset: 20,
+        soloArrowRightVOffset: 0,
+        shadow: 0,
+        fullWidth: "on",
+        fullScreen: "off",
+        spinner: "spinner4",
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+        shuffle: "off",
+        autoHeight: "off",
+        forceFullWidth: "on",
+        hideTimerBar: "on",
+        hideThumbsOnMobile: "on",
+        hideNavDelayOnMobile: 1500,
+        hideBulletsOnMobile: "on",
+        hideArrowsOnMobile: "on",
+        hideThumbsUnderResolution: 0,
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        startWithSlide: 0,
+    });
+
+
+
 		// Google Map Scripts//
   	    function init_map(){
   		    var iconBase = 'assets/images/icons/';
